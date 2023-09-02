4e78f2383d8fa0a0a3eb0bee90aa5b92651532fd
Roven Kyle Narra <ellamateogang@gmail.com>
2023-09-03 03:14:40 +0800

    additions for nav to be created

---
+++ src/screens/home/statistics.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Header } from 'screens/contents/components/gen/header'
+import Navbarmenu from 'screens/contents/components/gen/navigator/navbarmenu'
+
+type Props = {}
+
+export default function Statistics({}: Props) {
+  return (
+    <div>
+    <Header menu={Navbarmenu}/>
+    </div>
+  )
+}
